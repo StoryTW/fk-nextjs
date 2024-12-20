eac754f119be0807dc596c2c1e292fbefe745840
parienko_sergej <parienko_sergej@enot.work>
2024-12-20 03:14:51 +0300

    fix

--- src/components/Online/Online.tsx
+++ src/components/Online/Online.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { AnchorHTMLAttributes, FC } from 'react';
 import styles from './Online.module.scss';
+import clsx from 'clsx';
 
-export const Online = () => {
+interface IOnline extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  descriptionColor?: 'black' | 'gray';
+}
+
+export const Online: FC<IOnline> = ({ className, descriptionColor = 'gray', ...props }) => {
   return (
-    <a href='#' target='_blank' className={styles.online}>
+    <a href='#' target='_blank' className={clsx(styles.online, className)} {...props}>
       <span className={styles.text}>ON-LINE</span>
-      <span className={styles.description}>онлайн-консультация</span>
+      <span className={clsx(styles.description, styles[descriptionColor])}>
+        онлайн-консультация
+      </span>
     </a>
   );
 };
--- src/pageComponents/GuaranteesPage/GuaranteesPage.tsx
+++ src/pageComponents/GuaranteesPage/GuaranteesPage.tsx
@@ -1,13 +1,17 @@
 import { TransparentHeader } from '@/layout/TransparentHeader/TransparentHeader';
 import React from 'react';
+import styles from './GuaranteesPage.module.scss';
+import { Online } from '@/components/Online/Online';
 
 export const GuaranteesPage = () => {
   return (
-    <>
-      <TransparentHeader />
-      <main className='container'>
-          ada
-      </main>
-    </>
+    <div className='container'>
+      <div className={styles.root}>
+        <TransparentHeader />
+        <main>
+          <Online className={styles.online} descriptionColor='black' />
+        </main>
+      </div>
+    </div>
   );
 };
