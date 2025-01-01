36eafcc3e3e6603b591585e8f1eb2fcd62f607fc
Alphis <alphis@MacBook-Air-Alphis.local>
2025-01-01 15:18:41 +0300

    last fix

--- src/sections/BetSection/BetSection.tsx
+++ src/sections/BetSection/BetSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './ClientsSection.module.scss';
+import styles from './BetSection.module.scss';
 import Image from 'next/image';
 
 const liMassive = [
