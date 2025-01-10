66ae3f0617236c13822413938b3a60d261744a54
roman_restov <roman_restov@enot.work>
2025-01-10 18:42:32 +0300

    add project-news

--- src/pageComponents/ProjectNewsPage/ProjectNewsPage.tsx
+++ src/pageComponents/ProjectNewsPage/ProjectNewsPage.tsx
@@ -3,24 +3,15 @@ import React from 'react';
 import styles from './ProjectNewsPage.module.scss';
 import { Online } from '@/components/Online/Online';
 import { SocialsBlock } from '@/components/SocialsBlock/SocialsBlock';
+import { SwiperNews } from '@/sections/SwiperNews/SwiperBlock';
 
 export const ProjectNewsPage = () => {
   return (
     <div className='container'>
       <TransparentHeader />
       <main className={styles.root}>
-        <Online className={styles.online} descriptionColor='black' />
-        <div className={styles.wrapper}>
-          <p className={styles.text}>
-            На текущий момент на рынке обменников, работающих с рублем происходит бум трафика.
-            Многие игроки сходят с дистанции, или отходят от работы с рублем, <br />
-            Из-за активной борьбы регулятора с нелегальными казино и букмекерами, под раздачу
-            невольно попадают и обменные пункты, работающие по всем канонам KYC и AML, так как они
-            используют те же платежные каналы что и нелегалы. <br /> Мы продолжаем обслуживать
-            клиентов в прежнем режиме, но временно вводим ужесточенные правила фильтрации и фрода
-            для минимизации рисков вовлечения в мошеннические операции на фоне роста обьемов.
-          </p>
-        </div>
+        <Online className={styles.online} descriptionColor='black' />    
+        <SwiperNews />
         <SocialsBlock className={styles.socials} />
       </main>
     </div>
