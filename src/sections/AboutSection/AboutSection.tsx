f649b35b54b4fe4db5cfa49424d395766eda9df7
roman_restov <roman_restov@enot.work>
2025-01-14 16:57:33 +0300

    fix

--- src/sections/AboutSection/AboutSection.tsx
+++ src/sections/AboutSection/AboutSection.tsx
@@ -49,33 +49,35 @@ const DATA = [
 export default function AboutSection() {
   return (
     <section id='#about' className={styles.aboutUs}>
-      <h2 className={styles.title}>
-        О НАС
-        <Image
-          src={'/images/about/orange_light_line.png'}
-          alt='line'
-          width={84}
-          height={17}
-          className={styles.line}
-        />
-      </h2>
-      <div className={styles.content}>
-        <Image
-          src={'/images/about/about-img.png'}
-          alt='img'
-          width={323}
-          height={556}
-          className={styles.img}
-        />
-        <ul className={styles.list}>
-          {DATA.map((item) => {
-            return (
-              <li key={item.id} className={styles.item}>
-                {item.text}
-              </li>
-            );
-          })}
-        </ul>
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>
+          О НАС
+          <Image
+            src={'/images/about/orange_light_line.png'}
+            alt='line'
+            width={84}
+            height={17}
+            className={styles.line}
+          />
+        </h2>
+        <div className={styles.content}>
+          <Image
+            src={'/images/about/about-img.png'}
+            alt='img'
+            width={323}
+            height={556}
+            className={styles.img}
+          />
+          <ul className={styles.list}>
+            {DATA.map((item) => {
+              return (
+                <li key={item.id} className={styles.item}>
+                  {item.text}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </section>
   );
