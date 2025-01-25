b7d2ec8b77e3e53cb6882b7cf8a176498d18ed4c
Alphis <alphis@MacBook-Air-Alphis.local>
2025-01-25 19:07:14 +0300

    save

--- src/sections/BetSection/BetSection.tsx
+++ src/sections/BetSection/BetSection.tsx
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import styles from './BetSection.module.scss';
 import Image from 'next/image';
 import { liMassive, liMassive2, tableData } from './data';
+import Shedule from './Shedule/Shedule';
+import { Crypto, Ecommerce, Exchanger, Wallet } from './Shedule/data';
 
 export default function BetSection() {
   const [countMounth, setCountMounth] = useState<number>(0);
@@ -11,60 +13,78 @@ export default function BetSection() {
   const [type, setType] = useState<string>(tableData[0].typeProject);
   const [betAquaring, setBetAquaring] = useState<string>('');
   const [betApi, setBetApi] = useState<string>('');
+  const [dataShedule, setDataShedule] = useState<any>([
+    {
+      turnover: 0, // Случайное целое в диапазоне
+      percent: 0,
+    },
+  ]);
 
   const setEcommerce = () => {
     if (countMounth === 0 && countMounth <= 100000) {
       if (url) {
         setBetAquaring('6,6 - 8,6%');
         setBetApi('6,0 - 6,8%');
+        setDataShedule(Ecommerce.option1.withUrl);
       } else {
         setBetAquaring('6,6 - 8,4%');
         setBetApi('5,9 - 6,7%');
+        setDataShedule(Ecommerce.option1.withoutUrl);
       }
     }
     if (countMounth === 100000 && countMounth <= 500000) {
       if (url) {
         setBetAquaring('6,5 - 8,3%');
         setBetApi('5,6 - 6,6%');
+        setDataShedule(Ecommerce.option2.withUrl);
       } else {
         setBetAquaring('6,5 - 8,3%');
         setBetApi('5,6 - 6,6%');
+        setDataShedule(Ecommerce.option2.withoutUrl);
       }
     }
     if (countMounth === 500000 && countMounth <= 1000000) {
       if (url) {
         setBetAquaring('6,0 - 8,2%');
         setBetApi('3,0 - 4,2%');
+        setDataShedule(Ecommerce.option3.withUrl);
       } else {
         setBetAquaring('6,3 - 8,3%');
         setBetApi('5,6 - 6,5%');
+        setDataShedule(Ecommerce.option3.withoutUrl);
       }
     }
     if (countMounth === 1000000 && countMounth <= 5000000) {
       if (url) {
         setBetAquaring('5,9 - 8,0%');
         setBetApi('3,0 - 4,0%');
+        setDataShedule(Ecommerce.option4.withUrl);
       } else {
         setBetAquaring('6,0 - 8,2%');
         setBetApi('3,0 - 4,2%');
+        setDataShedule(Ecommerce.option4.withoutUrl);
       }
     }
     if (countMounth === 5000000 && countMounth <= 10000000) {
       if (url) {
         setBetAquaring('5,8 - 8,0%');
         setBetApi('3,0 - 4,0%');
+        setDataShedule(Ecommerce.option5.withUrl);
       } else {
         setBetAquaring('5,8 - 7,8%');
         setBetApi('3,0 - 4,0%');
+        setDataShedule(Ecommerce.option5.withoutUrl);
       }
     }
     if (countMounth >= 10000000) {
       if (url) {
         setBetAquaring('5,7 - 7,8%');
         setBetApi('3,0 - 3,9%');
+        setDataShedule(Ecommerce.option6.withUrl);
       } else {
         setBetAquaring('5,8 - 8,0%');
         setBetApi('3,0 - 4,0%');
+        setDataShedule(Ecommerce.option6.withoutUrl);
       }
     }
   };
@@ -76,36 +96,44 @@ export default function BetSection() {
           if (url) {
             setBetAquaring('5,2 - 6,3%');
             setBetApi('4,7 - 5,2%');
+            setDataShedule(Exchanger.option1.withUrl);
           } else {
             setBetAquaring('5,3 - 6,5%');
             setBetApi('4,7 - 5,3%');
+            setDataShedule(Exchanger.option1.withoutUrl);
           }
         }
         if (countMounth === 1000000 && countMounth <= 10000000) {
           if (url) {
             setBetAquaring('5,1 - 6%');
             setBetApi('4,5 - 5%');
+            setDataShedule(Exchanger.option2.withUrl);
           } else {
             setBetAquaring('5,2 - 6,3%');
             setBetApi('4,5 - 5,2%');
+            setDataShedule(Exchanger.option2.withoutUrl);
           }
         }
         if (countMounth === 10000000 && countMounth <= 100000000) {
           if (url) {
             setBetAquaring('5,0 - 5,9%');
             setBetApi('4,3 - 4,8%');
+            setDataShedule(Exchanger.option3.withUrl);
           } else {
             setBetAquaring('5,1 - 6%');
             setBetApi('4,4 - 5%');
+            setDataShedule(Exchanger.option3.withoutUrl);
           }
         }
         if (countMounth >= 100000000) {
           if (url) {
             setBetAquaring('4,9 - 6,0%');
             setBetApi('4,2 - 4,8%');
+            setDataShedule(Exchanger.option4.withUrl);
           } else {
             setBetAquaring('5,0 - 5,9%');
             setBetApi('4,3 - 4,8%');
+            setDataShedule(Exchanger.option4.withoutUrl);
           }
         }
         break;
@@ -115,36 +143,44 @@ export default function BetSection() {
           if (url) {
             setBetAquaring('5,2 - 6,3%');
             setBetApi('4,5 - 5,2%');
+            setDataShedule(Crypto.option1.withUrl);
           } else {
             setBetAquaring('5,3 - 6,5%');
             setBetApi('4,5 - 5,3%');
+            setDataShedule(Crypto.option1.withoutUrl);
           }
         }
         if (countMounth === 1000000 && countMounth <= 10000000) {
           if (url) {
             setBetAquaring('5,1 - 6%');
             setBetApi('4,4 - 5%');
+            setDataShedule(Crypto.option2.withUrl);
           } else {
             setBetAquaring('5,2 - 6,3%');
             setBetApi('4,5 - 5,2%');
+            setDataShedule(Crypto.option2.withoutUrl);
           }
         }
         if (countMounth === 10000000 && countMounth <= 100000000) {
           if (url) {
             setBetAquaring('5,0 - 5,9%');
             setBetApi('4,3 - 4,8%');
+            setDataShedule(Crypto.option3.withUrl);
           } else {
             setBetAquaring('5,1 - 6%');
             setBetApi('4,4 - 5%');
+            setDataShedule(Crypto.option3.withoutUrl);
           }
         }
         if (countMounth >= 100000000) {
           if (url) {
             setBetAquaring('4,9 - 6,0%');
             setBetApi('4,2 - 4,8%');
+            setDataShedule(Crypto.option4.withUrl);
           } else {
             setBetAquaring('5,0 - 5,9%');
             setBetApi('4,3 - 4,8%');
+            setDataShedule(Crypto.option4.withoutUrl);
           }
         }
         break;
@@ -154,36 +190,44 @@ export default function BetSection() {
           if (url) {
             setBetAquaring('6,6 - 8,6%');
             setBetApi('6,0 - 6,8%');
+            setDataShedule(Wallet.option1.withUrl);
           } else {
             setBetAquaring('6,6 - 8,4%');
             setBetApi('5,9 - 6,7%');
+            setDataShedule(Wallet.option1.withoutUrl);
           }
         }
         if (countMounth === 1000000 && countMounth <= 10000000) {
           if (url) {
             setBetAquaring('6,5 - 8,3%');
             setBetApi('5,6 - 6,6%');
+            setDataShedule(Wallet.option2.withUrl);
           } else {
             setBetAquaring('6,5 - 8,3%');
             setBetApi('5,6 - 6,6%');
+            setDataShedule(Wallet.option2.withoutUrl);
           }
         }
         if (countMounth === 10000000 && countMounth <= 100000000) {
           if (url) {
             setBetAquaring('6,1 - 7,0%');
             setBetApi('5,5 - 6,2%');
+            setDataShedule(Wallet.option3.withUrl);
           } else {
             setBetAquaring('6,3 - 8,3%');
             setBetApi('5,6 - 6,5%');
+            setDataShedule(Wallet.option3.withoutUrl);
           }
         }
         if (countMounth >= 100000000) {
           if (url) {
             setBetAquaring('5,9 - 7,0%');
             setBetApi('5,5 - 6,0%');
+            setDataShedule(Wallet.option4.withUrl);
           } else {
             setBetAquaring('6,1 - 7,0%');
             setBetApi('5,5 - 6,2%');
+            setDataShedule(Wallet.option4.withoutUrl);
           }
         }
         break;
@@ -211,13 +255,7 @@ export default function BetSection() {
         />
       </h2>
       <div className={styles.content}>
-        <Image
-          src={'/images/clients/tarifs.png'}
-          width={650}
-          height={400}
-          alt='tarif'
-          className={styles.contentImg}
-        />
+        <Shedule data={dataShedule} />
         <div>
           <h2 className={styles.subtitle}>
             Поможем сформировать условия именно под Вашу модель работы
@@ -274,10 +312,11 @@ export default function BetSection() {
                 <input
                   type='number'
                   onChange={(e) => setCountMounth(Number(e.currentTarget.value))}
+                  placeholder='0'
                 />
               </td>
               <td>
-                <input type='number' />
+                <input type='number' placeholder='0' />
               </td>
               <td>
                 <input
